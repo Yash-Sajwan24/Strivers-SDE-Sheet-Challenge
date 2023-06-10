29e9ba292de5d73ece84e1ed93d654200dde0393
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-06-11 03:51:47 +0530

    next permutation added '

---
+++ Arrays Part 1/Next Permutation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &nums, int n)
+{
+
+    int idx = -1;
+    for(int i = n-2;i>=0;i--){
+        if(nums[i] < nums[i+1]){
+            idx = i;
+            break;
+        }
+    }
+
+    if(idx == -1){
+        reverse(nums.begin(), nums.end());
+        return nums;
+    }
+
+    for(int i=n-1;i>idx;i--){
+        if(nums[i] > nums[idx]){
+            swap(nums[i], nums[idx]);
+            break;
+        }
+    }
+
+    sort(nums.begin() + idx + 1, nums.end());
+    return nums;
+}
